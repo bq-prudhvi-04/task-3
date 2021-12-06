96daaa84d4d09b4e841fe6e2448866c894110051
prudhvibetaque <94161894+prudhvibetaque@users.noreply.github.com>
2021-12-06 10:46:11 +0530

    Update ALB_target_group.tf

--- modules/Load-balancer/ALB_target_group.tf
+++ modules/Load-balancer/ALB_target_group.tf
@@ -3,6 +3,16 @@ resource "aws_lb_target_group" "test-target-gp" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+  
+   health_check {
+    interval            = 10
+    path                = "/"
+    protocol            = "HTTP"
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+  }
+  
 
 }
 
